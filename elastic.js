fb45d255872a1babbd994fa0ab00e797c4293c39
kruthikaG07 <68156933+kruthikaG07@users.noreply.github.com>
2020-09-06 11:33:18 -0400

    Add files via upload

---
+++ elastic.js
@@ -0,0 +1,28 @@
+class Elastic{
+    constructor(bodyA,pointB){
+   var options={
+bodyA:bodyA,
+pointB:pointB,
+length:10,
+stiffness:0.004
+   }
+   this.pointB = pointB;
+   this.elastic= Constraint.create(options)
+   World.add(world,this.elastic);
+    }
+    fly(){
+        this.elastic.bodyA=null;
+    }
+    
+    display(){
+        if(this.elastic.bodyA){
+       var  pointA = this.elastic.bodyA.position;
+       var  pointB = this.pointB;
+        strokeWeight(3);
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+        }
+    attach(body){
+        this.elastic.bodyA = body;
+    }
+}
